3b7c7400fff912561803c270afcb99a0438b4acc
Robert Karl <robertkarl@users.noreply.github.com>
2018-01-08 21:05:24 -0800

    removed some lines of code

--- vis.py
+++ vis.py
@@ -4,29 +4,17 @@ import matplotlib.pyplot as plt
 import random
 
 
-def save_stuff(array_to_vis):
+def save_arraylike_to_numpy(array_to_vis, fname):
     arraystuff = np.array(array_to_vis) 
-    arraystuff.tofile("floats.float32")
-    dims = arraystuff.shape
-    with open("floats.json", 'w') as outfile:
-        json.dump({"dims":dims}, outfile)
+    np.save(fname, arraystuff)
     return arraystuff
 
-def readstuff():
-    bts = None
-    with open("floats.float32", 'rb') as fp:
-        bts = fp.read()
-
-    with open("floats.json") as fp:
-        info = fp.read()
-        dims = json.loads(info)
-        dims = dims["dims"]
-    bts = np.fromstring(bts, 'float')
-    stuff = bts.reshape(dims)
+def read_and_visualize(fname):
+    stuff = np.load(fname)
     plt.matshow(stuff)
     return stuff
 
 
 if __name__ == "__main__":
-    readstuff()
+    read_and_visualize("floats.npy")
 
