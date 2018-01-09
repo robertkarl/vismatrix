628dbd3981e3fd7744f3bcb1395d5fea5471e38f
Robert Karl <robertkarl@users.noreply.github.com>
2018-01-08 20:54:48 -0800

    Save any data in array or ndarray format and then visualize it

--- vis.py
+++ vis.py
@@ -1,6 +1,16 @@
 import json
 import numpy as np
 import matplotlib.pyplot as plt
+import random
+
+
+def save_stuff(array_to_vis):
+    arraystuff = np.array(array_to_vis) 
+    arraystuff.tofile("floats.float32")
+    dims = arraystuff.shape
+    with open("floats.json", 'w') as outfile:
+        json.dump({"dims":dims}, outfile)
+    return arraystuff
 
 def readstuff():
     bts = None
